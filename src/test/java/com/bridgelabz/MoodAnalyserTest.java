d00a30a4afe2cf88b69975f1eb579231b7f222e8
nayana005 <nayanasuresh1005@gmail.com>
2022-09-10 00:28:11 +0530

    Added: Mood Analyser Test

---
+++ src/test/java/com/bridgelabz/MoodAnalyserTest.java
@@ -0,0 +1,25 @@
+package com.bridgelabz;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class MoodAnalyserTest 
+{
+	
+	@Test
+	public void givenMessage_WhenSad_ShouldReturnSad()
+	{
+		MoodAnalyser  moodAnalyser = new MoodAnalyser();
+		String mood = moodAnalyser.analyseMood("I am in sad");
+		Assert.assertEquals("SAD", mood);
+	}
+	
+	@Test
+	public void givenMessage_WhenNotSad_ShouldReturnHappy()
+	{
+		MoodAnalyser moodAnalyser = new MoodAnalyser();
+		String mood = moodAnalyser.analyseMood("This is a happy message");
+		Assert.assertEquals("HAPPY", mood);
+	}
+
+}
