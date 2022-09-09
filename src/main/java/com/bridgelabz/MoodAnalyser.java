1c208e36aad2fb5d4edf4bed2dd1d8870f1856c5
NAYANA S <102555432+nayana005@users.noreply.github.com>
2022-09-10 00:45:23 +0530

    Happy or Sad

--- src/main/java/com/bridgelabz/MoodAnalyser.java
+++ src/main/java/com/bridgelabz/MoodAnalyser.java
@@ -2,17 +2,6 @@ package com.bridgelabz;
 
 public class MoodAnalyser {
 
-	String message;
-	//Parameterize constructor
-	public MoodAnalyser(String message)
-	{
-		this.message = message;
-	}
-	//default constructor
-	public MoodAnalyser()
-	{
-		super();
-	}
 	//Method to return mood based on message
 	public String analyseMood(String message)
 	{
@@ -25,5 +14,6 @@ public class MoodAnalyser {
 			return "HAPPY";
 		}
 	}
-}
+}	
+	
 
