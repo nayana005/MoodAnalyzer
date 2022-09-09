e4ac9f902bddd139123061e0db6932e68a5e2dd2
NAYANA S <102555432+nayana005@users.noreply.github.com>
2022-09-10 00:31:51 +0530

    Mood Analyser Test

--- src/test/java/com/bridgelabz/MoodAnalyserTest.java
+++ src/test/java/com/bridgelabz/MoodAnalyserTest.java
@@ -10,7 +10,7 @@ public class MoodAnalyserTest
 	public void givenMessage_WhenSad_ShouldReturnSad()
 	{
 		MoodAnalyser  moodAnalyser = new MoodAnalyser();
-		String mood = moodAnalyser.analyseMood("I am in sad");
+		String mood = moodAnalyser.analyseMood("This is a sad message");
 		Assert.assertEquals("SAD", mood);
 	}
 	
